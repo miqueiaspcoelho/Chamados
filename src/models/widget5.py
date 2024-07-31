72fd75d6969ecdf6a50485d2e7912c2311c1405f
miqueiaspcoelho <miqueias.pereira.coelho@gmail.com>
2024-07-31 09:23:36 -0300

    mudança de layout nas opções de status, layout organizado para colunas e adição de mais opções

--- src/models/widget5.py
+++ src/models/widget5.py
@@ -9,10 +9,38 @@ do chamado desejado, pois, é nomal que com o passar do tempo eles alteram seu s
 class Widget5:
     @staticmethod
     def show():
+    
+        column1 = [
+            [sg.Radio('Resolvido', group_id='status',key='Resolvido',default=True)],
+            [sg.Radio('Aguardando',group_id='status',key='Aguardando')],
+            [sg.Radio('Pendente',group_id='status',key='Pendente')]
+        ]
+        
+        column2 = [
+            [sg.Radio('Manutenção Preventiva',group_id='status',key='Manutenção Preventiva')],
+            [sg.Radio('Manutenção Corretiva',group_id='status',key='Manutenção Corretiva')],
+            [sg.Text('')]
+        ]
+        
+        column3 = [
+            [sg.Radio('Troca',group_id='status',key='Troca')],
+            [sg.Radio('Reparo',group_id='status',key='Reparo')],
+            [sg.Text('')]
+        ]
+        
+        column4 = [
+            [sg.Radio('Pedido Efetuado',group_id='status',key='Pedido Efetuado')],
+            [sg.Radio('Pedido Recebido',group_id='status',key='Pedido Recebido')],
+            [sg.Text('')]
+        ]
         widget5 = [
             [sg.Text('Status', tooltip='Marque a atual condição da questão')],
-            [sg.Radio('Resolvido', group_id='status',key='Resolvido',default=True),sg.Radio('Pendente',group_id='status',key='Pendente'), sg.Radio('Manutenção',group_id='status',key='Manutenção'),sg.Radio('Troca',group_id='status',key='Troca') ],
-            [sg.Radio('RealJet Aberto', group_id='status',key='RealJet Aberto'),sg.Radio('RealJet Resolvido', group_id='status',key='RealJet Resolvido')],
+            [
+            sg.Column(column1),
+            sg.Column(column2),
+            sg.Column(column3),
+            sg.Column(column4)
+            ],
             [sg.HorizontalSeparator(color='gray')]
             ]
         return widget5
