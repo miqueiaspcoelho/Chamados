8e44c16c8d795ac2166c2126e72ec4c109fca0ae
miqueiaspcoelho <miqueias.pereira.coelho@gmail.com>
2024-07-31 09:56:13 -0300

    modificando tamanho da listbox que exibe os setores

--- src/models/widget1.py
+++ src/models/widget1.py
@@ -11,7 +11,7 @@ class Widget1:
                 [sg.Text('Data do atendimento: '),sg.Input(key='day', size=(3,1),do_not_clear=False),sg.Text('/'),sg.Input(key='month',size=(3,1),do_not_clear=False),sg.Text('/'),sg.Input(key='year',size=(5,1),do_not_clear=False)],
                 [sg.HorizontalSeparator(color='gray')],
                 [sg.Text('Selecione o Setor', tooltip='Setor de atendimento')],
-                [sg.Listbox(options, default_values=options[0], key='setor',size=(30, 7))], 
+                [sg.Listbox(options, default_values=options[0], key='setor',size=(40, 7))], 
                 [sg.HorizontalSeparator(color='gray')]
                 ]
         return widget1
