6cd9613195d1caca965a3b9a32dd9e94b248d2ef
miqueiaspcoelho <miqueias.pereira.coelho@gmail.com>
2024-07-31 08:55:49 -0300

    alteração em layout de itens que podem ser selecionados, acréscimo de mais opções

--- src/models/widget2.py
+++ src/models/widget2.py
@@ -7,14 +7,22 @@ class Widget2:
     
     @staticmethod
     def show():
-        widget2 = [
-            [sg.Text('Selecione o tipo', tooltip='Marque em quais itens foi o problema')],
+        column1 = [
             [sg.Radio('Computador', group_id='item', key='Computador', default=True)],
-            [sg.Radio('Impressora',group_id='item',key='Impressora')],
             [sg.Radio('Rede', group_id='item',key='Rede')],
-            [sg.Radio('Teclado', group_id='item',key='Teclado')],
             [sg.Radio('Mouse', group_id='item',key='Mouse')],
-            [sg.Radio('Telefone', group_id='item', key='Telefone')],
-            [sg.HorizontalSeparator(color='gray',key='item6')]
-            ]
+            [sg.Radio('Office', group_id='item', key='Office')]
+        ]
+        
+        column2 = [
+            [sg.Radio('Impressora', group_id='item', key='Impressora')],
+            [sg.Radio('Telefone', group_id='item',key='Telefone')],
+            [sg.Radio('Teclado', group_id='item',key='Teclado')],
+            [sg.Radio('Outro', group_id='item', key='Outro')]
+        ]
+        widget2 = [
+                [sg.Text('Selecione o tipo', tooltip='Marque em quais itens foi o problema')],
+                [sg.Column(column1), sg.Column(column2)],
+                [sg.HorizontalSeparator(color='gray')]
+        ]
         return widget2
