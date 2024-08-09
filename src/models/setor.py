de605ea42052fd7094b09366605d894f0546ae2b
miqueiaspcoelho <miqueias.pereira.coelho@gmail.com>
2024-08-09 10:20:31 -0300

    docstring adicionadas a modelo setor

--- src/models/setor.py
+++ src/models/setor.py
@@ -1,6 +1,12 @@
 #classe setor
-
 class Setor:
     
-    def __init__(self, name: str):
+    def __init__(self, name: str) -> None:
+        """
+        Modelo do objeto setor, possuindo o atributo nome
+        
+        :param name: string
+        
+        :return None
+        """
         self.name = name.lower()
