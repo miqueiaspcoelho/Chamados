dd4b58521b4cab4a5a3ba3654556496483a46d58
miqueiaspcoelho <miqueias.pereira.coelho@gmail.com>
2024-08-09 09:34:13 -0300

    docstring adicionado no modelo chamado

--- src/models/chamado.py
+++ src/models/chamado.py
@@ -1,12 +1,23 @@
 #classe chamado
-
 class Chamado:
     
     def __init__(self, setor: str, 
                 data:str,
                 item: str, 
                 status: str, 
-                descricao: str):
+                descricao: str) -> None:
+        """
+        Modelo de objeto chamado
+        
+        :param setor: string
+        :param data: string
+        :param item: string
+        :param status: string
+        :param descricao: string
+        
+        return None
+        """
+        
         self.setor = setor.lower()
         self.data = data
         self.item = item.lower()
