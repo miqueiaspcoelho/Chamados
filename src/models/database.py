9e0ecd4d9b12332851b4834af583c63bf9e93f1e
miqueiaspcoelho <miqueias.pereira.coelho@gmail.com>
2024-08-09 09:44:15 -0300

    docstring adicionado em modelo database

--- src/models/database.py
+++ src/models/database.py
@@ -1,7 +1,17 @@
 import sqlite3
 from sqlite3 import Error
 class Database:
-    def __init__(self, name: str):
+    def __init__(self, name: str) -> None:
+        """
+        Criação do banco de dados, possui um único atributo nome.
+        A estrutura do banco está em databaseControler
+        try -> instancia o banco 
+        except -> em caso de erro informa o erro
+        
+        :param name: string
+        
+        :return None
+        """
         self.name = name
         try:
             conn = sqlite3.connect(self.name) #já irá instanciar o banco
