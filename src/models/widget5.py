76867a4a79ff1fdf346012d6877e6d375628f261
miqueiaspcoelho <miqueias.pereira.coelho@gmail.com>
2024-08-09 11:24:35 -0300

    update comentários widget5

--- src/models/widget5.py
+++ src/models/widget5.py
@@ -1,11 +1,5 @@
 import FreeSimpleGUI as sg
 #WIDGET5 - status
-'''cada registro de chamado precisa possuir um status, ou seja, se já foi concluído,
-se está pendente, se foi repassado para outra empresa e como está a situação.
-foi utilizada a mesma estratégia de radio buttons, dessa forma cada chamado possui um único status.
-em breve será necessário criar a função de update no banco, com base no Id ser possível atualizar o status
-do chamado desejado, pois, é nomal que com o passar do tempo eles alteram seu status'''
-
 class Widget5:
     @staticmethod
     def show() -> object:
