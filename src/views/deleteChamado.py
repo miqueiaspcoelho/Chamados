0e90df8bd15eff27eea9ccfe0b74b4f3b6af5fc1
miqueiaspcoelho <miqueias.pereira.coelho@gmail.com>
2024-08-14 09:03:52 -0300

    modificação de docstring em deleteChamado

--- src/views/deleteChamado.py
+++ src/views/deleteChamado.py
@@ -18,13 +18,22 @@ class Delete:
     @staticmethod
     def open_window_delete(title:str, database_name: str, input_label:str, input_key:str, button_name1:str, button_name_key1:str,button_name2:str, button_name_key2:str) -> None:
         """
-        Não recebe nenhum parâmetro de entrada. Renderiza uma janela onde fornece
-        ao usuário o campo Id, com a finalidade de após isso fazer a exclusão do
-        dado por meio do Id informado, dessa forma os erros ao excluir um chamado
-        são minimizados
+        Responsável por renderizar uma janela para exclusão de um chamado por meio do id.
+        Nessa janela é possível realizar a busca do chamado, minimizando assim erros de
+        exclusão.
+        Os parâmetros recebidos são de configuração para a renderização da janela.
+        Apenas o database_name é utilizado para conexão com o banco de dados.
 
-        :param None
-        :return void
+        :param title: string
+        :param database_name: string
+        :param input_label: string
+        :param input_key: string
+        :param button_name1: string
+        :param button_name_key1: string
+        :param button_name2: string
+        :param button_name_key2: string
+        
+        :return None
         """
         widget7 = Widget7.show(input_label, input_key, button_name1,button_name_key1,button_name2,button_name_key2)
         window = sg.Window(title, widget7, modal=True,element_justification='left')
